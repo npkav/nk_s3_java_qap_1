50b428f8041fc44d08711d0b3e5ef25e88a6ff6b
Nick <NickolasKavanagh@proton.me>
2025-01-29 15:38:27 -0330

    Problem #1: The Account Class

--- Account/Account.java
+++ Account/Account.java
@@ -1,5 +1,71 @@
-package Account;
+// Nickolas Kavanagh - SD12
+// Semester 3 - Advanced Programming (Java)
+// 2025/01/22 - 2025/01/30
 
+// Problem 1: The Account Class
+
+package Account;
 public class Account {
+    private String id;
+    private String name;
+    private int balance = 0;
     
+    
+    // initialize account details
+    public Account(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    
+    
+    // initialize account details w/ optional balance parameter
+    public Account(String id, String name, int balance) {
+        this(id, name);
+        setBalance(balance);
+    }
+    
+    
+    // return account details
+    public String getID() { return id; }
+    public String getName() { return name; }
+    public int getBalance() { return balance; }
+    
+    
+    // set balance
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+    
+    
+    // add amount to balance
+    public void credit(int amount) {
+        setBalance(getBalance() + amount);
+    }
+    
+    
+    // subtract amount from balance
+    public void debit(int amount) {
+        if (amount <= getBalance()) {
+            credit( - amount);
+        } else {
+            System.out.println("Error: Amount exceeded balance.");
+        }
+    }
+    
+    
+    // transfer amount from balance to another account
+    public void transferTo(Account other, int amount) {
+        if (amount <= getBalance()) {
+            debit(amount);
+            other.credit(amount);
+        } else {
+            System.out.println("Error: Amount exceeded balance.");
+        }
+    }
+    
+    
+    // return account details
+    public String toString() {
+        return String.format("Account ID: %s || Account Owner: %s || Account Balance: $%d", getID(), getName(), getBalance());
+    }
 }
--- Account/TestAccount.java
+++ Account/TestAccount.java
@@ -1,4 +1,24 @@
 package Account;
 public class TestAccount {
-    
+    public static void main(String[] args) {
+        // initialize accounts
+        Account acc1 = new Account("0118999881999119725", "Maurice Moss", 5000);
+        Account acc2 = new Account("0000000000000000003", "Jen Barber", 4000);
+        
+        
+        // print starting balances
+        System.out.println("Starting balances:");
+        System.out.println(acc1.toString());
+        System.out.println(acc2.toString());
+        
+        
+        // transfer funds from acc1 to acc2
+        acc1.transferTo(acc2, 1000);
+        
+        
+        // print post-transfer balances
+        System.out.println("\nPost-transfer balances:");
+        System.out.println(acc1.toString());
+        System.out.println(acc2.toString());
+    }
 }
