4d247b780faac2459d244f3d2d780ca4af0c9e31
Nick <NickolasKavanagh@proton.me>
2025-01-29 19:04:31 -0330

    Problem #2: The Date Class

--- Date/Date.java
+++ Date/Date.java
@@ -1,5 +1,45 @@
-package Date;
+// Nickolas Kavanagh - SD12
+// Semester 3 - Advanced Programming (Java)
+// 2025/01/22 - 2025/01/30
 
+// Problem 2: The Date Class
+
+package Date;
 public class Date {
+    // initialize date details
+    private int day;
+    private int month;
+    private int year;
     
+    
+    // initialize date details
+    public Date(int day, int month, int year) {
+        setDate(day, month, year);
+    }
+    
+    
+    // return date details
+    public int getDay() { return day; }
+    public int getMonth() { return month; }
+    public int getYear() { return year; }
+    
+    
+    // set date details
+    public void setDay(int day) { this.day = day; }
+    public void setMonth(int month) { this.month = month; }
+    public void setYear(int year) { this.year = year; }
+    
+    
+    // set date details
+    public void setDate(int day, int month, int year) {
+        setDay(day);
+        setMonth(month);
+        setYear(year);
+    }
+    
+    
+    // return date details
+    public String toString() {
+        return String.format("The date is %02d/%02d/%04d", getDay(), getMonth(), getYear());
+    }
 }
--- Date/TestDate.java
+++ Date/TestDate.java
@@ -1,5 +1,11 @@
 package Date;
 
 public class TestDate {
-    
+    public static void main(String[] args) {
+        // initialize date
+        Date date = new Date(29, 1, 2025);
+        
+        // print date
+        System.out.println(date.toString());
+    }
 }
